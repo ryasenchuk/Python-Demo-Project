ecb1016f6ecf52e8c4dc695dbf58e7b035c68d96
ryasenchuk <sergeyryasenchuk@gmail.com>
2022-11-03 21:29:59 +0200

    add index.html

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask, request
+import psycopg2
+from flask import Flask, request, render_template
 from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+db = "dbname=pdp user=postgres password=210613 "
+conn = psycopg2.connect(db)
+cur = conn.cursor()
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+@app.route('/test/')
+def test():
+    try:
+        cur.execute("""SELECT title from video""")
+        rows = cur.fetchall()
+        response = ''
+        my_list = []
+        for row in rows:
+            my_list.append(row[0])
+
+        return render_template('index.html',  my_list=my_list)
+    except Exception as e:
+        print(e)
+        return 'error'
